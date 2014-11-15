cb2e4b032cbc30514f6689f981c669c1858242e3
lenin pardo <pardo0392@gmail.com>
2014-11-15 17:13:25 -0500

    cambios lenin 31

--- model/Main.php
+++ model/Main.php
@@ -694,8 +694,8 @@ Inner Join evento ON evento.idevento = detalle_asistencia_alumno_tutoria.idevent
                         proyecto.periodo_ejecucion,
                         CONCAT(profesores.ApellidoPaterno,' ',profesores.ApellidoMaterno,' ',profesores.NombreProfesor) AS Responsable,
                         escuelaprofesional.DescripcionEscuela,
-                        estado_proyecto.descripcion,
-                        MAX(DISTINCT(control_proyecto.fecha)) as Fecha_Inicio,
+                        proceso_proyecto.nombre,
+                       0,
                     
                         proyecto.presupuesto,
               
@@ -707,20 +707,18 @@ Inner Join evento ON evento.idevento = detalle_asistencia_alumno_tutoria.idevent
                         0,
                        0,
                       0,
-                        estado_proyecto.idestado_proyecto
+                       proceso_proyecto.idproceso_proyecto
                         FROM
                         proyecto
                         INNER JOIN detalle_profesor_proy_fun ON proyecto.idproyecto = detalle_profesor_proy_fun.idproyecto
                         INNER JOIN profesores ON profesores.CodigoProfesor = detalle_profesor_proy_fun.CodigoProfesor
-                        INNER JOIN control_proyecto ON proyecto.idproyecto = control_proyecto.idproyecto
-                        INNER JOIN estado_proyecto ON control_proyecto.idestado_proyecto = estado_proyecto.idestado_proyecto
+                        INNER JOIN proceso_proyecto on(proceso_proyecto.idproceso_proyecto=proyecto.estado_proyecto)
                         INNER JOIN escuelaprofesional ON proyecto.CodigoEscuela=escuelaprofesional.CodigoEscuela
                         INNER JOIN tipo_proyecto ON proyecto.idtipo_proyecto = tipo_proyecto.idtipo_proyecto
                         INNER JOIN facultades ON escuelaprofesional.CodigoFacultad = facultades.CodigoFacultad
                         INNER JOIN linea_investigacion ON proyecto.idlinea_investigacion = linea_investigacion.idlinea_investigacion
                         INNER JOIN eje_tematico ON linea_investigacion.idejetematico = eje_tematico.idejetematico
-                        INNER JOIN grupo ON eje_tematico.idgrupo = grupo.idgrupo
-                        
+                        INNER JOIN grupo ON eje_tematico.idgrupo = grupo.idgrupo               
                         
                         where detalle_profesor_proy_fun.idfuncion=2 and proyecto.situacion=1
                         GROUP BY proyecto.idproyecto
@@ -742,8 +740,8 @@ Inner Join evento ON evento.idevento = detalle_asistencia_alumno_tutoria.idevent
                         proyecto.periodo_ejecucion,
                         CONCAT(profesores.ApellidoPaterno,' ',profesores.ApellidoMaterno,' ',profesores.NombreProfesor) AS Responsable,
                         escuelaprofesional.DescripcionEscuela,
-                        estado_proyecto.descripcion,
-                        MAX(DISTINCT(control_proyecto.fecha)) as Fecha_Inicio,
+                        proceso_proyecto.nombre,
+                       0,
                     
                         proyecto.presupuesto,
               
@@ -753,23 +751,22 @@ Inner Join evento ON evento.idevento = detalle_asistencia_alumno_tutoria.idevent
                         eje_tematico.nombre_ejetematico,
                         grupo.nombre_grupo,
                         0,
-                        0,
-                        0,
-                        estado_proyecto.idestado_proyecto
+                       0,
+                      0,
+                       proceso_proyecto.idproceso_proyecto
                         FROM
                         proyecto
                         INNER JOIN detalle_profesor_proy_fun ON proyecto.idproyecto = detalle_profesor_proy_fun.idproyecto
                         INNER JOIN profesores ON profesores.CodigoProfesor = detalle_profesor_proy_fun.CodigoProfesor
-                        INNER JOIN control_proyecto ON proyecto.idproyecto = control_proyecto.idproyecto
-                        INNER JOIN estado_proyecto ON control_proyecto.idestado_proyecto = estado_proyecto.idestado_proyecto
+                        INNER JOIN proceso_proyecto on(proceso_proyecto.idproceso_proyecto=proyecto.estado_proyecto)
                         INNER JOIN escuelaprofesional ON proyecto.CodigoEscuela=escuelaprofesional.CodigoEscuela
                         INNER JOIN tipo_proyecto ON proyecto.idtipo_proyecto = tipo_proyecto.idtipo_proyecto
                         INNER JOIN facultades ON escuelaprofesional.CodigoFacultad = facultades.CodigoFacultad
                         INNER JOIN linea_investigacion ON proyecto.idlinea_investigacion = linea_investigacion.idlinea_investigacion
                         INNER JOIN eje_tematico ON linea_investigacion.idejetematico = eje_tematico.idejetematico
-                        INNER JOIN grupo ON eje_tematico.idgrupo = grupo.idgrupo
-                                          
-                        where detalle_profesor_proy_fun.idfuncion=2 and proyecto.situacion=0
+                        INNER JOIN grupo ON eje_tematico.idgrupo = grupo.idgrupo               
+                        
+                        where detalle_profesor_proy_fun.idfuncion=2 and proyecto.situacion=1
                         GROUP BY proyecto.idproyecto
                             ";
 
